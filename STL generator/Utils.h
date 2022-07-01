214c67cd4ed5ab38c4d34e0c2f55f3a6f2622f57
MarvellousBee <63006301+MarvellousBee@users.noreply.github.com>
2022-07-01 23:46:08 +0200

    storm commit

--- STL generator/Utils.h
+++ STL generator/Utils.h
@@ -11,3 +11,19 @@ void add_vectors(std::vector<T>& lhs, std::vector<T>&& rhs)
 {
     lhs.insert(lhs.end(), rhs.begin(), rhs.end());
 }
+
+template <class T>//too lazy to remember/look up how to point to a function
+std::string test_all(T function)
+{
+    std::string out{ "" };
+    out.append(function({ 0.f,0.f,0.f }, { 1.f,1.f,1.f }));
+    out.append(function({ 0.f,2.f,0.f }, { -1.f,1.f,1.f }));
+    out.append(function({ 0.f,4.f,0.f }, { 1.f,-1.f,1.f }));
+    out.append(function({ 0.f,6.f,0.f }, { -1.f,-1.f,1.f }));
+
+    out.append(function({ 5.f,2.f,0.f }, { 1.f,1.f,-1.f }));
+    out.append(function({ 5.f,4.f,0.f }, { -1.f,1.f,-1.f }));
+    out.append(function({ 5.f,6.f,0.f }, { 1.f,-1.f,-1.f }));
+    out.append(function({ 5.f,8.f,0.f }, { -1.f,-1.f,-1.f }));
+    return out;
+}
--- STL generator/main.cpp
+++ STL generator/main.cpp
@@ -13,23 +13,7 @@
 #include "SkinStructure.h"
 #include "BodyPart.h"
 #include "stl_templates.h"
-
-template <class T>//too lazy to remember/look up how to point to a function
-std::string test_all(T function)
-{
-    std::string out{""};
-    out.append(function({ 0.f,0.f,0.f }, { 1.f,1.f,1.f }));
-    out.append(function({ 0.f,2.f,0.f }, { -1.f,1.f,1.f }));
-    out.append(function({ 0.f,4.f,0.f }, { 1.f,-1.f,1.f }));
-    out.append(function({ 0.f,6.f,0.f }, { -1.f,-1.f,1.f }));
-
-    out.append(function({ 5.f,2.f,0.f }, { 1.f,1.f,-1.f }));
-    out.append(function({ 5.f,4.f,0.f }, { -1.f,1.f,-1.f }));
-    out.append(function({ 5.f,6.f,0.f }, { 1.f,-1.f,-1.f }));
-    out.append(function({ 5.f,8.f,0.f }, { -1.f,-1.f,-1.f }));
-    return out;
-}
-
+//#include "Utils.h"
 int main()
 {
     std::vector<std::string> mc_skin_documentation{};
