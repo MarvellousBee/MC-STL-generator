817a32bd80ab4ce0584332d14a3beb8b9be13d32
MarvellousBee <63006301+MarvellousBee@users.noreply.github.com>
2022-08-05 12:03:52 +0200

    Increase unit test coverage for XValues

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -11,23 +11,53 @@ TEST(TestXValues, Values4) {
     // const overload
     EXPECT_EQ(cref[0], 1);
 
-    EXPECT_TRUE(values4 == Values4(1, 2, 3, 4));
+    EXPECT_TRUE (values4 == Values4(1, 2, 3, 4));
+
+    EXPECT_FALSE(values4 == Values4(0, 2, 3, 4));
+    EXPECT_FALSE(values4 == Values4(1, 0, 3, 4));
+    EXPECT_FALSE(values4 == Values4(1, 2, 0, 4));
+    EXPECT_FALSE(values4 == Values4(1, 2, 3, 0));
+
+    
 }
 
-TEST(TestXValues, Point3f) {
-    Point3f p1{ 1,2,3 };
-    Point3f p2{ 4,5,6 };
+TEST(TestXValues, Point3) {
+    Point3 p1{ 1,2,3 };
+    Point3 p2{ 4,5,6 };
 
     ASSERT_EQ(p1.x, 1);
     ASSERT_EQ(p1.y, 2);
     ASSERT_EQ(p1.z, 3);
 
-    EXPECT_EQ(p1.sum(), 6);
+    Point3 sub{ p2 - p1 };
+    EXPECT_EQ(sub.x, 3);
+    EXPECT_EQ(sub.y, 3);
+    EXPECT_EQ(sub.z, 3);
 
-    p1 *= p2;
-    EXPECT_EQ(p1.x, 4);
-    EXPECT_EQ(p1.y, 10);
-    EXPECT_EQ(p1.z, 18);
+    Point3 mul{ p2 * p1 };
+    EXPECT_EQ(mul.x, 4);
+    EXPECT_EQ(mul.y, 10);
+    EXPECT_EQ(mul.z, 18);
+
+    auto p1_copy{ p1 };
+    p1_copy *= p2;
+    EXPECT_EQ(p1_copy.x, 4);
+    EXPECT_EQ(p1_copy.y, 10);
+    EXPECT_EQ(p1_copy.z, 18);
+
+    Point3 div_l{ 3, 6, 9 };
+    Point3 div{ div_l / 3 };
+
+    EXPECT_EQ(div.x, 1);
+    EXPECT_EQ(div.y, 2);
+    EXPECT_EQ(div.z, 3);
+
+    Point3 add{ p2 + p1 };
+    EXPECT_EQ(add.x, 5);
+    EXPECT_EQ(add.y, 7);
+    EXPECT_EQ(add.z, 9);
+
+    EXPECT_EQ(p1.sum(), 6);
 
     EXPECT_FALSE(p1 == p2);
 }
